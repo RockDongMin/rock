caee95b1578f57252013f02f1ba880f63181ed7f
rockdongmin <rockdongmin@gmail.com>
2024-11-24 21:38:20 +0900

    Update get_next_line.c

--- 01.inner circle/m1.get_next_line/get_next_line.c
+++ 01.inner circle/m1.get_next_line/get_next_line.c
@@ -6,12 +6,28 @@
 /*   By: rockdongmin <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 16:48:50 by rockdongmin       #+#    #+#             */
-/*   Updated: 2024/11/24 17:12:26 by rockdongmin      ###   ########.fr       */
+/*   Updated: 2024/11/24 21:14:31 by rockdongmin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_get(char **strs)
+{
+	int	i;
+}
+
+char	*ft_next(int columns)
+{
+
+}
+
+char	*ft_line(char *str)
+{
+
+}
+
 char	*get_next_line(int fd)
 {
+
 }
